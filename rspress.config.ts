0b670dd490665bb5b77bc090f027f680ba077407
Jacob-Valor <jacob-programmer@tuta.io>
2025-07-31 13:41:50 +0700

    update feature theme

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,13 +3,17 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'My Site',
-  icon: '/rspress-icon.png',
+  title: 'JACOB',
+  icon: '/JACOB-Icon.png',
+  logoText: 'JACOB',
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
+  globalStyles: path.join(__dirname, 'src/styles/Styles.css'),
+  
   themeConfig: {
+
     socialLinks: [
       { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
       { icon: 'x', mode: 'link', content: 'https://example.com' },
@@ -17,5 +21,8 @@ export default defineConfig({
       { icon: 'instagram', mode: 'link', content: 'https://www.instagram.com/yourusername' },
       { icon: 'youtube', mode: 'link', content: 'https://www.youtube.com/channel/yourchannel' },
     ],
+    footer: {
+      message: 'BLOG BY JACOB 2025 👾',
+    },
   },
 });
